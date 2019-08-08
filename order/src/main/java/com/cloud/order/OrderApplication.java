95a8a1b8c1a3dee0a2dafd304e6edda5ff02631b
wangzhongyi666 <a563978354>
2019-08-08 15:05:20 +0800

    333

--- order/src/main/java/com/cloud/order/OrderApplication.java
+++ order/src/main/java/com/cloud/order/OrderApplication.java
@@ -1,15 +1,13 @@
 package com.cloud.order;
 
-import com.cloud.frameworkbase.model.ConsulConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /*@EnableEurekaClient*/
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableConfigurationProperties({ConsulConfig.class})
+//@EnableConfigurationProperties({ConsulConfig.class})
 public class OrderApplication {
 
     public static void main(String[] args) {
